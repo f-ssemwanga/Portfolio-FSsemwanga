aba3cba3478a70cae455680277d1c96ac82f9697
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-04-03 17:58:03 +0100

    Added sections to the portfolio and more styles

---
+++ assets/scripts/controller.js
@@ -0,0 +1,27 @@
+
+const introSection = document.querySelector("#intro-section");
+const aboutSection = document.querySelector("#about-section");
+const skillsSection = document.querySelector("#skills-section");
+
+introSection.classList.add('show')
+aboutSection.classList.add('show');
+
+// const eportBtn = document.querySelector('.eport');
+// const homeLink = document.querySelector("a[href='#intro_section']");
+// const aboutLink = document.querySelector("a[href='#about-section']");
+// function showSection(sectionToShow, sectionToHide1){
+//     sectionToShow.style.display="block";
+//     sectionToHide1.style.display="none";
+//     // sectionToHide2.style.display="none";
+    
+// }
+
+// eportBtn.addEventListener('click', function(){
+//    showSection(aboutSection,introSection)
+// });
+// aboutLink.addEventListener('click',function(){
+//     showSection(aboutSection,introSection)
+// })
+// homeLink.addEventListener('click',function(){
+//     showSection(introSection,aboutSection)
+// })
